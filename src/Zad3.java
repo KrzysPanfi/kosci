da58bdc36ce1bdd725683eb1abfa125bc4936fa8
Krzysztof Panfilewicz <krzyspanfilewicz@gmail.com>
2024-02-10 12:33:25 +0100

    Reformat codu1

--- src/Zad3.java
+++ src/Zad3.java
@@ -1,7 +1,5 @@
 import java.util.Stack;
-
 public class Zad3 {
-
     public static String odwrocone_znaki(String exp) {
         Stack<Character> stack = new Stack<>();
         char[] znaki = exp.toCharArray();
@@ -12,7 +10,6 @@ public class Zad3 {
         for (int j = 0; j < znaki.length; j++) {
             odwrocone_znaki[j] = stack.pop();
         }
-
 
         return new String(odwrocone_znaki);
     }
