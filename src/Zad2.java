a4da6d8293111858865963c3af28832ad0c99741
Krzysztof Panfilewicz <krzyspanfilewicz@gmail.com>
2024-02-11 12:28:00 +0100

    Try catch w zad2

--- src/Zad2.java
+++ src/Zad2.java
@@ -9,29 +9,34 @@ public class Zad2 {
 
         for (int i = 0; i < znaki.length; i++) {
 
-            char current = znaki[i];
+          try {
+              char current = znaki[i];
 
-            if (current == '{' || current == '[' || current == '(') {
-                stack.push(current);
-            }
-            char popChar;
-            switch (current) {
-                case ')':
-                    popChar = stack.pop();
-                    if (popChar == '{' || popChar == '[')
-                        return false;
-                    break;
-                case '}':
-                    popChar = stack.pop();
-                    if (popChar == '(' || popChar == '[')
-                        return false;
-                    break;
-                case ']':
-                    popChar = stack.pop();
-                    if (popChar == '(' || popChar == '{')
-                        return false;
-                    break;
-            }
+              if (current == '{' || current == '[' || current == '(') {
+                  stack.push(current);
+              }
+              char popChar;
+              switch (current) {
+                  case ')':
+                      popChar = stack.pop();
+                      if (popChar == '{' || popChar == '[')
+                          return false;
+                      break;
+                  case '}':
+                      popChar = stack.pop();
+                      if (popChar == '(' || popChar == '[')
+                          return false;
+                      break;
+                  case ']':
+                      popChar = stack.pop();
+                      if (popChar == '(' || popChar == '{')
+                          return false;
+                      break;
+              }
+          }
+          catch (Exception e){
+              return false;
+          }
         }
         return stack.empty();
 
